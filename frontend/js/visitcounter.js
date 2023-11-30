52ca83e9b5982019ec7f87e560f33fa642860a16
Laurence James-Woodley <mr.lawjames@gmail.com>
2023-11-30 09:17:00 -0500

    add dummy frontend

---
+++ frontend/js/visitcounter.js
@@ -0,0 +1,15 @@
+const functionApi = 'https://counter31jsr2b0.azurewebsites.net/api/visit_counter?code=UawQdFkYwkZ3ep5I4PAVrQEmE-urbPsatgKkyv93pnf7AzFuUaIDWg==';
+
+fetch(functionApi)
+    .then(response => {
+        return response.json();
+    })
+    .then (response => {
+        console.log('Fetch succeeded to function.');
+        document.getElementById('counter').innerText = response;
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+
+ 
